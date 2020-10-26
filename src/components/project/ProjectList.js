c7a2c172b87d6c2a15f51765575766bc73685779
Amr Adel <amradel21@icloud.com>
2020-10-26 17:49:00 +0200

    first commit

---
+++ src/components/project/ProjectList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+ const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Amr Adel</p>
+                    <p className="grey-text">21th October 11:00pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Farida Salem</p>
+                    <p className="grey-text">22th October 11:00pm</p>
+                </div>
+            </div>
+
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Maram Hussein</p>
+                    <p className="grey-text">23th October 11:00pm</p>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+export default ProjectList;
